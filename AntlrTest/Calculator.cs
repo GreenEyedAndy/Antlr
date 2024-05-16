d029370e56b3ca515ca916e761d46e1bf7931a1c
Andy <apatock@gmx.de>
2024-05-16 22:01:07 +0200

    Das geht irgendwie - durch das installierte Tool.

---
+++ AntlrTest/Calculator.cs
@@ -0,0 +1,28 @@
+﻿namespace AntlrTest;
+
+public class Calculator : ExprParserBaseVisitor<object>
+{
+    public override object VisitStat(ExprParser.StatContext context)
+    {
+        Console.WriteLine(context.GetText());
+        return context.GetText();
+    }
+
+    public override object VisitDef(ExprParser.DefContext context)
+    {
+        Console.WriteLine(context.GetText());
+        return context.GetText();
+    }
+
+    public override object VisitExpr(ExprParser.ExprContext context)
+    {
+        Console.WriteLine(context.GetText());
+        return context.GetText();
+    }
+
+    public override object VisitFunc(ExprParser.FuncContext context)
+    {
+        Console.WriteLine(context.GetText());
+        return context.GetText();
+    }
+}
---
+++ AntlrTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace AntlrTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string input = """
+                       f(x,y) {
+                           a = 3+4;
+                           x and y;
+                       }
+                       """;
+        ICharStream stream = CharStreams.fromString(input);
+        ITokenSource lexer = new ExprLexer(stream);
+        ITokenStream tokens = new CommonTokenStream(lexer);
+        ExprParser parser = new ExprParser(tokens);
+        IParseTree tree = parser.program();
+        Calculator calculator = new Calculator();
+        var output = calculator.Visit(tree);
+        Console.WriteLine(output);
+    }
+}
