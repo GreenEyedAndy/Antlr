13b9d80b121937eb82ca455014f50cb9d7aff989
Andreas Patock <apatock@gmx.de>
2024-05-22 16:56:37 +0200

    Console-App und Classlib vorbereitet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var filter = UrlFilterToBsonDocument.UrlFilterConverter.ConvertToBson(
+            """((name eq Andy or name eq "Igor") and _age gte 0.46) and flag eq true or lastModified lt 2023-07-19T14:35:31.347Z""");
+        Console.ReadLine();
+    }
+}
